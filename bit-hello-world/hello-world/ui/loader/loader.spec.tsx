b5f16f02aa86a121ab38a5af87447d136904959a
Sidnei Filho <sidneifilho123@gmail.com>
2023-05-25 17:33:29 -0300

    feat: create ui/loader react component

---
+++ bit-hello-world/hello-world/ui/loader/loader.spec.tsx
@@ -0,0 +1,9 @@
+// import React from 'react';
+// import { render } from '@testing-library/react';
+// import { BasicLoader } from './loader.composition';
+
+// it('renders with the correct text', () => {
+//   const { getByText } = render(<BasicLoader />);
+//   const rendered = getByText('hello world!');
+//   expect(rendered).toBeTruthy();
+// });
