a9e98e4f2b319c4e8b2e19e8bd835eaa11779946
Sidnei Filho <sidneifilho123@gmail.com>
2023-05-25 22:25:13 -0300

    feat: removed node_modules

--- node-server/index.js
+++ node-server/index.js
@@ -1,2 +1,7 @@
 const app = require('./app')
 module.exports = app;
+
+// status service ms
+// const { expressApp } = require('@sidneicsfilho/ms-demo.status-service')
+// module.exports = expressApp;
+
