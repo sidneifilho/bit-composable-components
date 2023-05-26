915d084ed053a197f9370e7a6c9a854dc22235ec
Sidnei Filho <sidneifilho123@gmail.com>
2023-05-26 11:16:05 -0300

    feat: add route is-up in node-server

--- node-server/app.js
+++ node-server/app.js
@@ -1,24 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const helloRouter = require('./routes/hello');
-const goodbyeRouter = require('@sidneicsfilho/hello-world.routes.goodbye')
-const isupRouter = require('@learnbit/microservices.routes.is-up')
+const { getIsUpRoute } = require('@sidneicsfilho/ms-demo.routes.is-up')
 const port = process.env.PORT || 4000;
 
 const app = express();
 app.use(cors());
 
 app.use('/api', helloRouter);
-app.use('/api', goodbyeRouter);
+
+const isUpRoute = getIsUpRoute()
+app[isUpRoute.method](isUpRoute.route, isUpRoute.middlewares)
 
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
-});
-
-const { checkOkStatus } = require('@learnbit/microservices.network-profilers.check-ok-status');
-
-checkOkStatus('https://google.com').then((isUp) => {
-  console.log(isUp);
 });
 
 module.exports = app;
