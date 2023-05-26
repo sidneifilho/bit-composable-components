b2813430c35edb9a0a0204fb929359e3f849e276
Sidnei Filho <sidneifilho123@gmail.com>
2023-05-25 22:35:12 -0300

    fix: ajustado testes do componente production urls

--- bit-status-service-ws/ms-demo/production-urls/production-urls.spec.ts
+++ bit-status-service-ws/ms-demo/production-urls/production-urls.spec.ts
@@ -1,5 +1,17 @@
 import { productionUrls } from './production-urls';
 
 it('should return the correct value', () => {
-  expect(productionUrls()).toBe('Hello world!');
+  const urls = {
+    'community-site': {
+      url: 'https://bit.dev',
+      description: 'our community site',
+      id: 'community',
+    },
+    'cloud-site': {
+      url: 'https://bit.cloud',
+      description: 'our cloud site',
+      id: 'cloud',
+    },
+  }
+  expect(productionUrls()).toStrictEqual(urls);
 });
